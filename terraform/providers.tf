06c92e2538a24e080310d218dc0169759530f08c
anuruddhamani <35539599+anuruddhamani@users.noreply.github.com>
2025-03-25 11:50:45 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.AZURE_SUBSCRIPTION_ID
+  client_id       = var.AZURE_AD_CLIENT_ID
+  client_secret   = var.AZURE_AD_CLIENT_SECRET
+  tenant_id       = var.AZURE_AD_TENANT_ID
+}
+
 terraform {
   required_providers {
     azurerm = {
@@ -6,6 +14,8 @@ terraform {
     }
   }
 }
-provider "azurerm" {
-  features {}
-}
+
+variable "53a046b6-93b5-4ac5-a86a-2a587677e5e1" {}
+variable "6b2aefb3-46f5-463a-9f44-12227b9ea08b" {}
+variable "jhgjgjhghjg" {}
+variable "44942895-564f-4b65-8948-39b0f3174c57" {}
